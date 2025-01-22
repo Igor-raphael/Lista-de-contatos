0b3b53b1507efcafa220906931a9ce19f80bbf57
Igor Rafael <igorcoelho808@gmail.com>
2025-01-21 23:09:40 -0300

    first commit

---
+++ src/main/java/br/com/igorRafael/ListaDeContatos/ListaDeContatosApplication.java
@@ -0,0 +1,13 @@
+package br.com.igorRafael.ListaDeContatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaDeContatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaDeContatosApplication.class, args);
+	}
+
+}
